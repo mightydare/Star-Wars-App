7f6dbad4f5ef0a1e12ac08e995338862d445000d
Darko <darko.stojanov@yahoo.com>
2020-07-18 05:31:13 +0200

    Initial Commit

---
+++ StarWarsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsApp
+//
+//  Created by Darko Stojanov on 7/18/20.
+//  Copyright © 2020 Darko Stojanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
